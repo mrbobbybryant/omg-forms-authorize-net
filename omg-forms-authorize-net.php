6a9902700d3f32b56ed663a49bf1613ff3d5d721
Bobby Bryant <bobby@Bobbys-MacBook-Pro-2.local>
2017-11-15 12:09:20 -0500

    replace hidden field with register_supplementary_field

--- omg-forms-authorize-net.php
+++ omg-forms-authorize-net.php
@@ -28,13 +28,13 @@ function authorize_net_force_rest( $args ) {
 add_filter( 'omg_form_filter_register_args', 'authorize_net_force_rest' );
 
 function authorize_net_filter_form_fields( $args ) {
-	if ( $args[ 'form_type' ] === 'authorize_net' ) {
-		$args['fields'][] = [
-			'slug' =>   'transaction_amount',
-			'type'  =>  'hidden',
-			'group' =>  'group_3'
-		];
-	}
+	$args = \OMGForms\Core\register_supplementary_field(
+		'authorize_net', [
+		'slug' =>   'transaction_amount',
+		'type'  =>  'hidden'
+		],
+		$args
+	);
 
 	return $args;
 }
