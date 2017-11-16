abe64a905d80e0f81774ef3ff0d0a19bc03e3dee
Bobby Bryant <bobby@Bobbys-MacBook-Pro-2.local>
2017-11-16 10:44:24 -0500

    update WP_Error instances, add in card validation in processing operation

--- includes/api.php
+++ includes/api.php
@@ -9,6 +9,11 @@ function save_form_as_authorize_net( $result, $args, $form ) {
 	if ( $form[ 'form_type' ] === 'authorize_net' ) {
 
 		$data = Helpers\prepare_authorize_net_form_fields( $args );
+		$data = Helpers\format_expiration_date( $data );
+
+		if ( is_wp_error( $data ) ) {
+			return $data;
+		}
 
 		$result = ProcessCard\process_card( $data );
 
--- includes/process-card.php
+++ includes/process-card.php
@@ -88,23 +88,21 @@ function process_card( $data ) {
 
 		$tresponse = $response->getTransactionResponse();
 		if ( ( $tresponse != null ) && ( $tresponse->getResponseCode() == "1" ) ) {
-			return $tresponse;
-		} else {
+			return true;
+		} else if ( $tresponse != null ) {
 			if ($tresponse->getErrors() != null) {
 				return TransactionErrors\handle_authorize_net_form_errors( $tresponse->getErrors()[0] );
-//				echo " Error Code  : " . $tresponse->getErrors()[0]->getErrorCode() . "\n";
-//				echo " Error Message : " . $tresponse->getErrors()[0]->getErrorText() . "\n";
 			}
-
-//			return new \WP_Error(
-//				'authorize-net-card-error',
-//				'Charge Credit Card ERROR :  Invalid response',
-//				array( 'status' => 400 )
-//			);
+		} else {
+			return new \WP_Error(
+				'omg-form-submission-error',
+				'Charge Credit Card Null response returned',
+				array( 'status' => 400 )
+			);
 		}
 	} else {
 		return new \WP_Error(
-			'authorize-net-card-error',
+			'omg-form-submission-error',
 			'Charge Credit Card Null response returned',
 			array( 'status' => 400 )
 		);
