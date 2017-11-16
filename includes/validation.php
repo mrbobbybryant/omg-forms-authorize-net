20dc11aebfa9f806cb85acb66e36bafb78559aaf
Bobby Bryant <bobby@Bobbys-MacBook-Pro-2.local>
2017-11-16 10:43:35 -0500

    add more logic to determine if a forms expiration date fields are valid

--- includes/validation.php
+++ includes/validation.php
@@ -18,8 +18,15 @@ function valid_authorize_net_forms( $args ) {
 		}, false );
 
 		if ( $errors ) {
-			throw new \Exception( 'Authorize.net form is missing the following required fields: ' . $errors );
+			trigger_error( 'Authorize.net form is missing the following required fields: ' . implode( ', ', $errors ), E_USER_ERROR );
 		}
+
+		$errors = validate_expiration_data_fields( $args['fields'] );
+
+		if ( $errors ) {
+			trigger_error( 'Authorize.net form is missing the following required fields: ' . implode( ', ', $errors ), E_USER_ERROR );
+		}
+
 	}
 }
 add_action( 'omg_form_validation', __NAMESPACE__ . '\valid_authorize_net_forms' );
@@ -32,4 +39,24 @@ function field_exists( $field, $fields ) {
 
 		return $prev;
 	}, false );
+}
+
+function validate_expiration_data_fields( $fields ) {
+	if ( field_exists( 'expiration_date', $fields ) ) {
+		return false;
+	} else {
+		$month = field_exists( 'expiration_month', $fields );
+		$year = field_exists( 'expiration_year', $fields );
+		$field_errors = [];
+
+		if ( ! $month ) {
+			$field_errors[] = 'expiration_month';
+		}
+
+		if ( ! $year ) {
+			$field_errors[] = 'expiration_year';
+		}
+
+		return ( empty( $field_errors ) ) ? false : $field_errors;
+	}
 }
