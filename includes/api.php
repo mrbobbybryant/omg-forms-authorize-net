d14b14f834a589c3851d032ba18c4bda0126ab91
Bobby Bryant <bobby@Bobbys-MacBook-Pro-2.local>
2017-11-21 14:21:38 -0500

    Update API to work with update core API - Endpoints now return the original data they were give or an error. - So endpoint has an additional check to see if the data it receives is an error

--- includes/api.php
+++ includes/api.php
@@ -6,35 +6,39 @@ use OMGForms\Authorize\Validation;
 use OMGForms\Authorize\ProcessCard;
 use OMGForms\Helpers as CoreHelpers;
 
-function save_form_as_authorize_net( $result, $args, $form ) {
+function save_form_as_authorize_net( $args, $form ) {
 
-	if ( CoreHelpers\is_form_type( 'authorize_net', $form ) ) {
-
-		$data = Helpers\prepare_authorize_net_form_fields( $args );
-
-		if ( is_wp_error( $data ) ) {
-			return $data;
-		}
-
-		$data = apply_filters( 'modify_omg_forms_authorize_data', $data, $args, $form );
-
-		$data = Helpers\format_authorize_net_form_fields( $data );
-
-		if ( is_wp_error( $data ) ) {
-			return $data;
-		}
-
-		$data = Validation\valid_card_information( $data );
-
-		if ( is_wp_error( $data ) ) {
-			return $data;
-		}
-
-		ProcessCard\process_card( $data, $form[ 'address' ] );
-
+	if ( ! CoreHelpers\is_form_type( 'authorize_net', $form ) || is_wp_error( $args ) ) {
+		return $args;
 	}
 
-	return Helpers\remove_sensitive_data( $result );
+	$data = Helpers\prepare_authorize_net_form_fields( $args );
+
+	if ( is_wp_error( $data ) ) {
+		return $data;
+	}
+
+	$data = apply_filters( 'modify_omg_forms_authorize_data', $data, $args, $form );
+
+	$data = Helpers\format_authorize_net_form_fields( $data );
+
+	if ( is_wp_error( $data ) ) {
+		return $data;
+	}
+
+	$data = Validation\valid_card_information( $data );
+
+	if ( is_wp_error( $data ) ) {
+		return $data;
+	}
+
+	$data = ProcessCard\process_card( $data, $form[ 'address' ] );
+
+	if ( is_wp_error( $data ) ) {
+		return $data;
+	}
+
+	return Helpers\remove_sensitive_data( $args );
 }
 
-add_filter( 'omg_forms_save_data', __NAMESPACE__ .  '\save_form_as_authorize_net', 10, 3 );
+add_filter( 'omg_forms_save_data', __NAMESPACE__ .  '\save_form_as_authorize_net', 10, 2 );
