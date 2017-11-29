f30d94d9dbd63ed56159f2f66b88ca8a58868caa
Bobby Bryant <bobby@Bobbys-MacBook-Pro-2.local>
2017-11-29 17:03:00 -0500

    bump version, bump omg-foms core version

--- omg-forms-authorize-net.php
+++ omg-forms-authorize-net.php
@@ -1,7 +1,7 @@
 <?php
 
 if ( !defined( 'OMG_FORMS_AUTHORIZE_VERSION' ) ) {
-	define( 'OMG_FORMS_AUTHORIZE_VERSION', '0.2.0' );
+	define( 'OMG_FORMS_AUTHORIZE_VERSION', '0.2.1' );
 }
 
 if ( !defined( 'OMG_FORMS_AUTHORIZE_DIR' ) ) {
